e1a48e2a041a130f88cba8110aa6198efe4baaa3
Afif-Afwan <68459591+Afif-Afwan@users.noreply.github.com>
2021-02-02 14:31:45 +0800

    Delete vendormanagement directory

--- vendormanagement/src/main/java/com/vendormanagement/data/VendorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vendormanagement.data;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.vendormanagement.model.Vendor;
-
-public interface VendorRepository extends CrudRepository<Vendor, Integer> {
-
-}
